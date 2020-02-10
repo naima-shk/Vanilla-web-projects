b3416d34e454f949121c93225e20b5f8468c2559
naima shaikh <naimashaikh550@gmail.com>
2020-02-10 15:57:40 +0500

    'new-folder'

---
+++ Form Validator/script.js
@@ -0,0 +1,14 @@
+const form = document.getElementById("form");
+const username = document.getElementById("username");
+const email = document.getElementById("email");
+const password = document.getElementById("password");
+const password2 = document.getElementById("password2");
+
+form.addEventListener("submit", function(e) {
+  e.preventDefault();
+  if (username.value === "") {
+    showError(username, "username is required");
+  } else {
+    showSuccess(username);
+  }
+});
