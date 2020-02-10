c5dc62cde1075b1edd92cdd88f2094775cbd784e
naima shaikh <naimashaikh550@gmail.com>
2020-02-10 16:10:42 +0500

    'new-folder'

--- Form Validator/script.js
+++ Form Validator/script.js
@@ -3,7 +3,12 @@ const username = document.getElementById("username");
 const email = document.getElementById("email");
 const password = document.getElementById("password");
 const password2 = document.getElementById("password2");
-
+//Show input error message
+function showError(input, message) {
+  const formControl = input.parentElement;
+  formControl.className = "form-control error";
+}
+//Event Listeners
 form.addEventListener("submit", function(e) {
   e.preventDefault();
   if (username.value === "") {
